07068fdd4a7b349bd92982d9126a0b316dbc0488
C-Macargo <caio.camargos97@gmail.com>
2023-02-22 19:50:59 -0300

    feat, sign up

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,32 @@
-export async function signUp(req, res) {}
+import { db } from "../configs/database.connection.js";
+import bcrypt from "bcrypt";
+
+export async function signUp(req, res) {
+	const { email, password, name } = req.body;
+	const hashedPassword = bcrypt.hashSync(password, 10);
+
+	try {
+		const emailExists = await db.query(
+			`SELECT * FROM users WHERE "email" = $1`,
+			[email]
+		);
+		if (emailExists.rows.length > 0) {
+			return res.status(400).send("email already in use!");
+		}
+
+		const user = await db.query(
+			`
+        	INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *
+        `,
+			[name, email, hashedPassword]
+		);
+		console.log(user.rows[0]);
+		return res.status(201).send("user added successfully");
+	} catch (err) {
+		console.log(err);
+		return res.status(500).send(err.message);
+	}
+}
 
 export async function signIn(req, res) {}
 
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,10 +1,11 @@
 import { signIn, signUp,myUser } from "../controllers/UserController.js"
 import { Router } from 'express'
-
+import { validateSchema } from "../middlewares/ValidateSchema.js";
+import { SignUpSchema } from "../schemas/SignUpSchema.js";
 
 const UserRouter = Router();
 
-UserRouter.post("/signup", signUp);
+UserRouter.post("/signup", validateSchema(SignUpSchema), signUp);
 UserRouter.post("/signin", signIn);
 UserRouter.post("/users/me", myUser);
 
