d7a4d0fe053743d289ddad58899570d367a9b942
C-Macargo <caio.camargos97@gmail.com>
2023-02-23 17:03:40 -0300

    feat,  post shorten

--- controllers/UrlController.js
+++ controllers/UrlController.js
@@ -1,4 +1,31 @@
-export async function shortenUrl(req, res) {}
+import { db } from "../configs/database.connection.js";
+import { customAlphabet } from "nanoid";
+
+export async function shortenUrl(req, res) {
+	const { url } = req.body;
+	const currentSession = res.locals.session;
+	const user = currentSession.rows[0].user_id;
+
+	const nanoid = customAlphabet("1234567890abcdef", 8);
+	const shortenedUrl = nanoid();
+
+	try {
+		const shorten = await db.query(
+			`
+        	INSERT INTO urls (user_id , url, short_url) VALUES ($1, $2, $3) RETURNING *
+        `,
+			[user, url, shortenedUrl]
+		);
+        const returning = shorten.rows[0]
+
+        const id = returning.id
+        const shortUrl = returning.short_url
+
+		return res.status(201).json({id,shortUrl});
+	} catch (err) {
+		return res.status(500).send(err);
+	}
+}
 
 export async function getUrl(req, res) {}
 
--- routes/UrlRoutes.js
+++ routes/UrlRoutes.js
@@ -1,10 +1,12 @@
 import { shortenUrl, getUrl, openUrl, deleteUrl } from "../controllers/UrlController.js"
 import { Router } from 'express'
-
+import { validateToken } from "../middlewares/ValidateToken.js";
+import { validateSchema } from "../middlewares/ValidateSchema.js";
+import { shortenUrlSchema } from "../schemas/ShortenUrlSchema.js";
 
 const UrlRouter = Router();
 
-UrlRouter.post("/urls/shorten", shortenUrl);
+UrlRouter.post("/urls/shorten",validateToken, validateSchema(shortenUrlSchema), shortenUrl);
 UrlRouter.get("/urls/:id", getUrl);
 UrlRouter.post("/urls/open/:shortUrl", openUrl);
 UrlRouter.delete("/urls/:id", deleteUrl);
